4467cb86ecb0d22123138008046bc3917c8ef4bd
AncutaBiro <ancuta.biro@yahoo.com>
2020-06-19 18:15:23 +0300

    Create contact on work and display agenda from back-end

--- js/agenda.js
+++ js/agenda.js
@@ -1,40 +1,74 @@
 window.PhoneBook = {
 
-API_URL: 'http://localhost:8081/agenda',
+  API_URL: 'http://localhost:8081/agenda',
 
   createAgenda: function () {
-  let firstNameValue = $('#firstName').val();
-  let lastNameValue = $('#lastName').val();
-  let phoneNumberValue = $('#phoneNumber').val();
-  let emailValue = $('#email').val();
+    let firstNameValue = $('#firstName').val();
+    let lastNameValue = $('#lastName').val();
+    let phoneNumberValue = $('#phoneNumber').val();
+    let emailValue = $('#email').val();
 
-  var requestBody = {
-    firstName: firstNameValue,
-    lastName: lastNameValue,
-    phoneNumber: phoneNumberValue,
-    email:emailValue,
-  };
-        $.ajax (
-          {
-            url:PhoneBook.API_URL,
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(requestBody)
-          }).done(function (response) {
-            console.log(response);
+    var requestBody = {
+      firstName: firstNameValue,
+      lastName: lastNameValue,
+      phoneNumber: phoneNumberValue,
+      email: emailValue,
+    };
+    $.ajax(
+      {
+        url: PhoneBook.API_URL,
+        method: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(requestBody)
+      }).done(function () {
+      PhoneBook.getAgenda();
+    });
+  },
 
-        })
+  getAgenda: function () {
+    $.ajax({
+      url: PhoneBook.API_URL,
+    }).done(function (response) {
+      PhoneBook.displayAgenda(JSON.parse(response));
+    });
+  },
+
+  displayAgenda: function (agenda) {
+    let rowsHtml = '';
+
+    agenda.forEach(agenda => rowsHtml += PhoneBook.getAgendaRowHtml(agenda));
+
+    $('.add-form tbody').html(rowsHtml);
+
+  },
+
+  getAgendaRowHtml: function (agenda) {
+    return `<tr>
+            <td>${agenda.firstName}</td>
+            <td>${agenda.lastName}</td>
+            <td>${agenda.phoneNumber}</td>
+            <td>${agenda.email}</td>
+            <td>
+              <a href="#" class="edit-contact" data-id=${agenda.id}>
+                <i class="fa fa-pencil-square" aria-hidden="true"></i>
+              </a>
+              <a href="#" class="remove-contact" data-id=${agenda.id}>
+                <i class="fa fa-trash"></i>
+              </a>
+            </td>
+          </tr>`
   },
 
   bindEvents: function () {
-  $('.add-form').submit(function (event) {
-    event.preventDefault();
-    PhoneBook.createAgenda();
-  });
-}
+    $('.add-form').submit(function (event) {
+      event.preventDefault();
+      PhoneBook.createAgenda();
+    });
+  }
 
 };
 
+PhoneBook.getAgenda();
 PhoneBook.bindEvents();
 
 
